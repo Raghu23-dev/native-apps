ec7532e18fc359c516ab36f9d22177bdfb2f2a7a
raghu23-dev <raghu2308.dev@gmail.com>
2024-08-21 16:06:33 +0530

    Adding modal

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,17 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, Modal } from 'react-native';
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import TextInputExample from '@/components/TestInputExample';
-import App from '@/components/Flatlist';
-
+import App from '@/components/Modal';
 
 export default function HomeScreen() {
   return (
     <>
-    <TextInputExample/>
+    {/* <TextInputExample/>
+    <App/> */}
     <App/>
+
     </>
   )}
--- components/Flatlist.tsx
+++ components/Flatlist.tsx
@@ -11,15 +11,15 @@ import {
 const DATA = [
   {
     id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'First Item',
+    title: 'User 1',
   },
   {
     id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
+    title: 'User 2',
   },
   {
     id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
+    title: 'User 3',
   },
 ];
 
